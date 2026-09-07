a428d04f1f46011a1a476c856d934798f77aa889
Henrico Kayz <henricokayz@gmail.com>
2026-09-07 12:53:00 +0300

    Modified

--- Scripts/getdates.js
+++ Scripts/getdates.js
@@ -1,7 +1,5 @@
 console.log()
 const currentYear = new Date().getFullYear();
 
-document.getElementById("currentyear").textContent = currentYear;
-
-document.getElementById("lastModified").textContent = 
-"Last Modification: " + document.lastModified;
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
