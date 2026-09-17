dcf06ae15b9a813c7d5e40bab983d97672043f87
Henrico Kayz <henricokayz@gmail.com>
2026-09-17 22:25:31 +0300

    Temple Album

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,15 @@
 const currentYear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
+
 const today = new Date();
+
 currentYear.textContent = today.getFullYear();
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
 
-const hambButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
 
-hambButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hambButton.classList.toggle('open');
-})
+const menuButton = document.querySelector("#menuButton");
+const navigation = document.querySelector("#menu");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
